4991c917fd2cbc7df70dc20d1842e2d59117bff4
johnsbcollins-lgtm <john.sb.collins@gmail.com>
2026-06-26 02:50:32 -0500

    claude fuck you

--- finance-backend/src/main/java/com/example/finance_backend/User.java
+++ finance-backend/src/main/java/com/example/finance_backend/User.java
@@ -13,7 +13,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
     @SequenceGenerator(name = "users_seq", sequenceName = "users_seq", allocationSize = 1)
-
+    private Long id;
+    
     private String email;
     private String password;
 
