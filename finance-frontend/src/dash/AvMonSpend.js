729f0a91f0740ba2a7d178cc708a6789c0bae040
johnsbcollins-lgtm <john.sb.collins@gmail.com>
2026-06-28 23:53:06 -0500

    actually fixed AvMonSpend

--- finance-frontend/src/dash/AvMonSpend.js
+++ finance-frontend/src/dash/AvMonSpend.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AvMonSpend({ total, months }) {
     return (
         <div>
-            {months && total > 0 && <h2>Average Monthly Expense: ${(total / months).toFixed(2)}</h2>}
+            {months > 0 && <h2>Average Monthly Expense: ${(total / months).toFixed(2)}</h2>}
         </div>
     );
 }
