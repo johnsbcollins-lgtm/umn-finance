c03b5dc0221b88f992443d9e237db37b03e0f453
johnsbcollins-lgtm <john.sb.collins@gmail.com>
2026-06-29 21:37:13 -0500

    forgot to change api_url

--- finance-frontend/src/config.js
+++ finance-frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8081'
+export const API_URL = 'https://umn-finance-production.up.railway.app'
 /*
 const localUrl ='http://localhost:8081'
  apiUrl = 'https://umn-finance-production.up.railway.app'
