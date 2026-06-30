08e5bd20589c6a9fa1ef743ac864ac1c0386e424
johnsbcollins-lgtm <john.sb.collins@gmail.com>
2026-06-30 16:28:45 -0500

    s

--- finance-frontend/src/config.js
+++ finance-frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL
+export const API_URL = process.env.REACT_APP_API_URL;
 console.log("API_URL:", API_URL);
 /*
 const localUrl ='http://localhost:8081'
