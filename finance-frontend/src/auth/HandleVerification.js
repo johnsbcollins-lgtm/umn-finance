95f6a58df70256e43f982c2ad3ed2affbd568a25
johnsbcollins-lgtm <john.sb.collins@gmail.com>
2026-06-30 19:00:09 -0500

    navigate fix

--- finance-frontend/src/auth/HandleVerification.js
+++ finance-frontend/src/auth/HandleVerification.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { API_URL } from '../config';
 function HandleVerification() {
     const [searchParams] = useSearchParams();
     const token = searchParams.get('token');
     const navigate = useNavigate();
-    function verifyEmail() {
+
+    useEffect(() => {
         fetch(`${API_URL}/auth/verify?token=${token}`,{
             method: 'POST',
             })
@@ -21,11 +22,10 @@ function HandleVerification() {
         })
         .catch(error => {console.error(error);});
             
-    }
+    }, [token, navigate]);
     return(
         <div>
             <h2>Loading...</h2>
-            {verifyEmail()}
         </div>
     )
 }
